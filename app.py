549faa699469c330bff9dbdada0b07fe2ed653c6
mmaithani <mohitmaithani81@gmail.com>
2020-07-25 16:28:33 +0530

    tested the server

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
+# import pandas as pd
+# from flask import Flask, jsonify, request
+# import pickle
+# # load model
+# model = pickle.load(open('model.pkl','rb'))
+# # app
+# app = Flask(__name__)
+
+# # routes
+# @app.route('/', methods=['POST'])
+
+# def predict():
+#     # get data
+#     data = request.get_json(force=True)
+
+#     # convert data into dataframe
+#     data.update((x, [y]) for x, y in data.items())
+#     data_df = pd.DataFrame.from_dict(data)
+
+#     # predictions
+#     result = model.predict(data_df)
+
+
+#     # send back to browser
+#     output = {'results': int(result[0])}
+
+#     # return data
+#     return jsonify(results=output)
+
+# if __name__ == '__main__':
+#     app.run(port = 8000, debug=True)
+
 import pandas as pd
 from flask import Flask, jsonify, request
 import pickle
 
-
 # load model
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('modell.pkl','rb'))
 
 # app
 app = Flask(__name__)
@@ -23,7 +54,6 @@ def predict():
     # predictions
     result = model.predict(data_df)
 
-
     # send back to browser
     output = {'results': int(result[0])}
 
@@ -32,4 +62,3 @@ def predict():
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
-
